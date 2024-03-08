b6136788b14d4ee554bd6dc0655020ba8e1ee01f
da9dac <da9dac@gmail.com>
2024-03-08 22:26:58 +0900

    Update 카드 구매하기.java

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -12,15 +12,12 @@ public class Main {
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		dp[1] = arr[1];
-
-		for (int i = 2; i <= n; i++) {
+            		dp[i] = Integer.parseInt(st.nextToken());
 			for (int j = 1; j <= i; j++) {
-				dp[i] = Math.max(dp[i], arr[j] + dp[i - j]);
+				dp[i] = Math.max(dp[i], dp[j] + dp[i - j]);
 			}
 		}
 
