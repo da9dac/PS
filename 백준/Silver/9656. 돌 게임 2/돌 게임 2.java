755c186f4100dfb28c6d6580809cb6c0be5f5b9a
da9dac <da9dac@gmail.com>
2024-03-14 08:55:12 +0900

    [Silver V] Title: 돌 게임 2, Time: 84 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        System.out.println(n % 2 == 0 ? "SK" : "CY");
+    }
+}
