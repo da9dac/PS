2931fbd302c9a136b5558127551d51cdc3117fc4
da9dac <da9dac@gmail.com>
2024-03-23 08:29:33 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1540 ms, Memory: 304236 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+			int count = n;
+			st = new StringTokenizer(br.readLine());
+
+			int[]students = new int[n];
+			int[] arr = new int[n];
+
+			boolean[] isCycle = new boolean[n];
+			boolean[] isNotCycle = new boolean[n];
+
+			for (int j = 0; j < n; j++) {
+				int select = Integer.parseInt(st.nextToken()) - 1;
+
+				students[j] = select;
+
+				if (j == select) {
+					isCycle[j] = true;
+					count--;
+				}
+			}
+
+			for (int j = 0; j < n; j++) {
+				if (isCycle[j] || isNotCycle[j]) continue;
+
+				int select = students[j];
+
+				arr[0] = j;
+
+				int size = 1;
+
+				while (true) {
+					if (isCycle[select] || isNotCycle[select]) {
+						for (int k = 0; k < size; k++) {
+							isNotCycle[arr[k]] = true;
+						}
+						break;
+					}
+
+					if (j == select) {
+						for (int k = 0; k < size; k++) {
+							isCycle[arr[k]] = true;
+						}
+						count -= size;
+						break;
+					}
+
+					if (size >= count) {
+						isNotCycle[j] = true;
+						break;
+					}
+
+					arr[size++] = select;
+					select = students[select];
+				}
+			}
+
+			sb.append(count).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
