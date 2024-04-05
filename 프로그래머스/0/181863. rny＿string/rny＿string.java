a19567e6483edebfe6ea72eeca936c173041b075
da9dac <da9dac@gmail.com>
2024-04-05 13:52:07 +0900

    [level 0] Title: rny_string, Time: 0.04 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,26 +1,37 @@
 class Solution {
     public String solution(String rny_string) {
-        int len = 0;
-        int idx = 0;
+//         int len = 0;
+//         int idx = 0;
         
-        char[] chars = rny_string.toCharArray();
+//         char[] chars = rny_string.toCharArray();
         
-        for (int i = 0; i < chars.length; i++) {
-            if (rny_string.charAt(i) == 'm') len += 2;
-            else len++;
+//         for (int i = 0; i < chars.length; i++) {
+//             if (rny_string.charAt(i) == 'm') len += 2;
+//             else len++;
+//         }
+        
+//         char[] rn = new char[len];
+        
+//         for (char c : chars) {
+//             if (c == 'm') {
+//                 rn[idx++] = 'r';
+//                 rn[idx++] = 'n';
+//             } else {
+//                 rn[idx++] = c;
+//             }
+//         }
+        
+//         return new String(rn);
+        
+        // return rny_string.replace("m", "rn");
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : rny_string.toCharArray()) {
+            if (c == 'm') sb.append("rn");
+            else sb.append(c);
         }
         
-        char[] rn = new char[len];
-        
-        for (char c : chars) {
-            if (c == 'm') {
-                rn[idx++] = 'r';
-                rn[idx++] = 'n';
-            } else {
-                rn[idx++] = c;
-            }
-        }
-        
-        return new String(rn);
+        return sb.toString();
     }
 }
