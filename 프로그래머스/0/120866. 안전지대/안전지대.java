b13ae1ecb500e801fda78d3847c2349e53ec01b4
da9dac <da9dac@gmail.com>
2024-04-06 21:55:12 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+        int safe = n * n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    safe--;
+                    for (int p = i - 1; p <= i + 1; p++) {
+                        if (p < 0 || p >= n) continue;
+                        
+                        for (int q = j - 1; q <= j + 1; q++) {
+                            if (q < 0 || q >= n) continue;
+                            
+                            if (board[p][q] == 0) {
+                                safe--;
+                                board[p][q] = 2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return safe;
+    }
+}
