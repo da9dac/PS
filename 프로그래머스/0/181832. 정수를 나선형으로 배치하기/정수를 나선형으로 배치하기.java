f022fc67e701f19e370c244fa8b3ca52f13adb0d
da9dac <da9dac@gmail.com>
2024-04-12 15:48:08 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,60 +1,46 @@
 class Solution {
     public int[][] solution(int n) {
-        int[][] answer =  new int[n][n];
+        int[][] answer = new int[n][n];
 
         int num = 1;
-        int go = 0;
-        int right = 0, left = n - 1, up = 0, down = n - 1;
-
-        while (num <= n * n){
-            switch (go) {
-                case 0:{
-                    for(int i = 0; i < n; i++) {
-                        if(answer[right][i] == 0) {
-                            answer[right][i] = num;
-                            num++;
-                        }
-                    }
-                    right++;
-                    go++;
-                    break;
-                }
-                case 1:{
-                    for(int i = 0; i < n; i++) {
-                        if(answer[i][down] == 0) {
-                            answer[i][down] = num;
-                            num++;
-                        }
-                    }
-                    down--;
-                    go++;
-                    break;
-                }
-                case 2:{
-                    for(int i = n - 1; i >= 0; i--) {
-                        if(answer[left][i] == 0) {
-                            answer[left][i] = num;
-                            num++;
-                        }
-                    }
-                    left--;
-                    go++;
-                    break;
-                }
-                case 3:{
-                    for(int i = n - 1; i >= 0; i--) {
-                        if(answer[i][up] == 0) {
-                            answer[i][up] = num;
-                            num++;
-                        }
-                    }
-                    up++;
-                    go = 0;
-                    break;
-                }
-
+        int end = n * n;
+        int x = 0, y = 0;
+        
+        int dir = 0;
+        
+        for (int i = 1; i <= end; i++) {
+            answer[x][y] = i;
+            
+            int nx = x;
+            int ny = y;
+            
+            if (dir == 0) {
+                ny++;
+            } else if (dir == 1) {
+                nx++;
+            } else if (dir == 2) {
+                ny--;
+            } else {
+                nx--;
+            }
+            
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n || answer[nx][ny] != 0) {
+                dir++;
+            }
+            
+            if (dir > 3) dir = 0;
+            
+            if (dir == 0) {
+                y++;
+            } else if (dir == 1) {
+                x++;
+            } else if (dir == 2) {
+                y--;
+            } else {
+                x--;
             }
         }
+        
         return answer;
     }
 }
