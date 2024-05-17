d3689cfef518e218fe4e6ca0b0c5f8bc17bbfdec
da9dac <da9dac@gmail.com>
2024-05-17 18:51:23 +0900

    [Silver I] Title: 점프, Time: 84 ms, Memory: 11768 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+
+		long[][] dp = new long[n][n];
+		dp[0][0] = 1;
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			for (int j = 0; j < n; j++) {
+				int x = Integer.parseInt(st.nextToken());
+				if (dp[i][j] == 0 ||( i == n - 1 && j == i)) continue;
+				if (i + x < n) dp[i + x][j] += dp[i][j];
+				if (j + x < n) dp[i][j + x] += dp[i][j];
+			}
+		}
+
+		System.out.println(dp[n - 1][n - 1]);
+	}
+}
