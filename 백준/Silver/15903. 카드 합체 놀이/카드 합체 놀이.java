b34864e96ac24ff2cc6a7ba6382f661ccab62e17
da9dac <da9dac@gmail.com>
2024-10-08 23:51:17 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 100 ms, Memory: 13060 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        
+        while (n-- > 0) pq.offer(Long.parseLong(st.nextToken()));
+        
+        while (m-- > 0) {
+            long sum = pq.poll() + pq.poll();
+            
+            pq.offer(sum);
+            pq.offer(sum);
+        }
+        
+        long sum = 0;
+        
+        while (!pq.isEmpty()) sum += pq.poll();
+        
+        System.out.println(sum);
+    }
+}
