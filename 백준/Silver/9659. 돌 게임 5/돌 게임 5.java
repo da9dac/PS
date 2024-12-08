a64cf1e68cb1cf081642395cfd0dd0e0ded335ce
da9dac <da9dac@gmail.com>
2024-12-08 23:37:15 +0900

    [Silver III] Title: 돌 게임 5, Time: 68 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
+		System.out.println(n % 2 == 1 ? "SK" : "CY");
+	}
+}
