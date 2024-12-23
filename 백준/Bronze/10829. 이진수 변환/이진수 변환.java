cec9bf825669b5bc85a7c522173b9c72b6b965f3
da9dac <da9dac@gmail.com>
2024-12-23 23:48:48 +0900

    [Bronze II] Title: 이진수 변환, Time: 68 ms, Memory: 11680 KB -BaekjoonHub

---
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.math.*;
+class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.println(new BigInteger(new BufferedReader(new InputStreamReader(System.in)).readLine()).toString(2));
+	}
+}
