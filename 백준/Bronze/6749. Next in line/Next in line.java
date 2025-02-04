727e96c50a19d58748ca1bb08bd36d3b9d536e97
da9dac <da9dac@gmail.com>
2025-02-04 23:45:35 +0900

    [Bronze IV] Title: Next in line, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        
+        System.out.println(y - x + y);
+    }
+}
