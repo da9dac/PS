0f6443d56a16402329b02091670c021e7b5a7a36
da9dac <da9dac@gmail.com>
2025-03-13 23:54:43 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int max = Math.max(b - a, c - b) - 1;
+        
+        System.out.println(max);
+    }
+}
