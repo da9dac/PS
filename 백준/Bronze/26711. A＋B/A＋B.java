72245cf93c28fa21354fe989b9a7ee21365efab5
da9dac <da9dac@gmail.com>
2025-04-01 19:38:13 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 19392 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		
+		System.out.println(a.add(b));
+		sc.close();
+	}
+}
