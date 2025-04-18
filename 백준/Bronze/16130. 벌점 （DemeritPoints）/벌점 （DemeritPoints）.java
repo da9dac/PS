18eb0fa29e8e797615e8d8afb0399f04656b71ab
da9dac <da9dac@gmail.com>
2025-04-19 00:14:03 +0900

    [Bronze I] Title: 벌점 (DemeritPoints), Time: 64 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Bronze/16130. 벌점 （DemeritPoints）/벌점 （DemeritPoints）.java
+++ 백준/Bronze/16130. 벌점 （DemeritPoints）/벌점 （DemeritPoints）.java
@@ -1,44 +1,41 @@
 import java.io.*;
 
 class Main {
+	static final String LIFE = "(weapon)";
+	static final String PERMANENT = "(09)";
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
-		String weapon = "(weapon)";
-		String nine = "(09)";
 
 		StringBuilder sb = new StringBuilder();
 
 		while (n-- > 0) {
-			String info = br.readLine();
-			int len = info.length();
-
 			int sum = 0;
 			int week = 0;
 			int flag = 0;
 
-			for (int i = 0; i < len; i++) {
-				char x = info.charAt(i);
+			for (char x : br.readLine().toCharArray()) {
 				int score = Character.isDigit(x) ? x - '0' : x - 55;
 
 				int prev = sum / 10;
 				sum += score;
 				int next = sum / 10;
 
-				if (prev < next) {
-					if (next < 4) {
-						week += next;
-					} else {
-						flag = (next == 4) ? 1 : 2;
-						break;
-					}
+				if (prev >= next) continue;
+
+				if (next >= 4) {
+					flag = next == 4 ? 1 : 2;
+					break;
 				}
+
+				week += next;
 			}
 
 			sb.append(week);
 
-			if (flag != 0) sb.append(flag == 1 ? weapon : nine);
+			if (flag != 0) sb.append(flag == 1 ? LIFE : PERMANENT);
 
 			sb.append("\n");
 		}
